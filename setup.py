99412b33eed4e2fb90a7bda2447a79074910dc0f
sylashalderb <sylashalderb@gmail.com>
2023-12-16 20:37:35 +0600

    Project for Registration and Login

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is about Login and Registration interface details. ',
+    author='Sylas Halder',
+    license='',
+)
